15c487140fdac23192152dd5f60653c0b6f95716
Tunapaw <peapea1@naver.com>
2021-02-26 14:15:19 +0900

    no message

--- IOS_toon/IOS_toon/EmailCheckModel.swift
+++ IOS_toon/IOS_toon/EmailCheckModel.swift
@@ -8,18 +8,18 @@
 import Foundation
 
 protocol JspModelProtocol: class {
-    func itemDownloaded(items: NSArray)
+    func itemDownloaded(items: String)
 }
 
 class EmailCheckModel{
     
-    var count: Int = 0
+
     var delegate: JspModelProtocol!
     var urlPath = "http://127.0.0.1:8080/iosproject/emailCheck.jsp"
     func checkItems(Email: String){
     let urlAdd = "?Email=\(Email)"
     urlPath = urlPath + urlAdd
-        
+        print("urlpath\(urlPath)")
         urlPath = urlPath.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
         let url = URL(string: urlPath)!
         let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
@@ -30,34 +30,42 @@ class EmailCheckModel{
             print("failed to download data")
         }else{
             print("Data is downloading")
-           // self.parseJONS(data!)
+            self.parseJONS(data!)
             }
         }
         task.resume()
     }
 
     
-//    func parseJONS(_ data: Data){
-//        if let data = contents.data(using: .utf-8) {
-//            let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
-//            persons = json
-//        }
-//        var countArr = [String]()
-//
-//        if let user_info = user_infos["user_info"] as? [[String: Any]]{
-//
-//            for personIndex in user_infos {
-//
-//                countArr.append(personIndex["name"] as! String)
-//
-//            }
-//
-//        }
-//
-//
-//
-//
-//
-//
-//    }
+    func parseJONS(_ data: Data){
+        var jsonResult = NSArray()
+
+
+        do{
+            jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! NSArray
+
+        }catch let error as NSError{
+            print(error)
+        }
+
+        var jsonElement = NSDictionary()
+        let locations = NSMutableArray()
+        var count: String = ""
+        for i in 0..<jsonResult.count{
+            jsonElement =  jsonResult[i] as! NSDictionary
+
+            if let countresult = jsonElement["count"] as? String{
+                locations.add(countresult)
+                
+                count = locations[0] as! String
+            }
+
+        }
+        DispatchQueue.main.async(execute: {() -> Void in
+            self.delegate?.itemDownloaded(items: count)
+            print("이메일체크모델\(count)")
+            
+        })
+    }
+
 }
--- IOS_toon/IOS_toon/JoinViewController.swift
+++ IOS_toon/IOS_toon/JoinViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class JoinViewController: UIViewController, JspModelProtocol {
+  
+    
 
 
     @IBOutlet weak var txtJoinName: UITextField!
@@ -19,109 +21,229 @@ class JoinViewController: UIViewController, JspModelProtocol {
     @IBOutlet weak var txtJoinTel: UITextField!
     
     
+    @IBOutlet weak var lblEmailCheck: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        txtJoinPassword.textContentType = .newPassword
-//        txtJoinPassword.isSecureTextEntry = true
-//        txtJoinPasswordCheck.textContentType = .newPassword
-//        txtJoinPasswordCheck.isSecureTextEntry = true
-      
-        let jspModel = EmailCheckModel()
-        jspModel.delegate = self
-        // let UEmail = txtJoinEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        // jspModel.checkItems(Email: UEmail)
-    }
-    
+                txtJoinPassword.textContentType = .newPassword
+                txtJoinPassword.isSecureTextEntry = true
+                txtJoinPasswordCheck.textContentType = .newPassword
+                txtJoinPasswordCheck.isSecureTextEntry = true
+                lblEmailCheck.text = "중복확인을 해주세요"
 
-    var feedItem: NSArray = NSArray()
-    
-    func itemDownloaded(items: NSArray) {
-        feedItem = items
     }
-    
-    @IBAction func btnJoinEmailCheck(_ sender: UIButton) {
-        let UEmail = txtJoinEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let emailCheckModel = EmailCheckModel()
-        _ = emailCheckModel.checkItems(Email: UEmail)
+
+
+        var feedItem: String = ""
         
-       
-        if feedItem.count == 0{
-            let resultAlert = UIAlertController(title: "성공", message: "사용가능한 아이디 입니다.", preferredStyle: UIAlertController.Style.alert)
-            let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
-            resultAlert.addAction(onAction)
-            present(resultAlert, animated: true, completion: nil)
-        }else{
-            let resultAlert = UIAlertController(title: "실패", message: "사용중인 아이디 입니다.", preferredStyle: UIAlertController.Style.alert)
-            let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {ACTION in
-                self.navigationController?.popViewController(animated: true)
-            })
-            resultAlert.addAction(onAction)
-            present(resultAlert, animated: true, completion: nil)
+
+        func itemDownloaded(items: String) {
+            feedItem = items
+            print("아이템다운로드\(feedItem)")
+            if Int(feedItem) == 0 || txtJoinEmail.text != nil{
+                let resultAlert = UIAlertController(title: "성공", message: "사용가능한 아이디 입니다.", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                resultAlert.addAction(onAction)
+                present(resultAlert, animated: true, completion: nil)
+                lblEmailCheck.text = ""
+            }else if Int(feedItem) == 1{
+                let resultAlert = UIAlertController(title: "실패", message: "사용중인 아이디 입니다.", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                resultAlert.addAction(onAction)
+                present(resultAlert, animated: true, completion: nil)
+                lblEmailCheck.text = "사용 불가능한 이메일입니다."
+                txtJoinEmail.text = ""
+                }
+        }
+        
+        
+        @IBAction func btnJoinEmailCheck(_ sender: UIButton) {
             
+            let emailCheckModel = EmailCheckModel()
+            emailCheckModel.delegate = self
+            emailCheckModel.checkItems(Email: txtJoinEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines))
+        }
+        
+
+     
+        
+        /*
+        // MARK: - Navigation
+
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // Get the new view controller using segue.destination.
+            // Pass the selected object to the new view controller.
+        }
+        */
+        
+        
+        @IBAction func btnJoin(_ sender: UIButton) {
+            let UEmail = txtJoinEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let UName = txtJoinName.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let UPassword = txtJoinPassword.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let UPasswordCheck = txtJoinPasswordCheck.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let UPostcode = txtJoinPostcode.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let UAddr = txtJoinAddr.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let UTel = txtJoinTel.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            func displayAlertMessage(userMessage: String){
+                let myAlert = UIAlertController(title: "알림", message: userMessage, preferredStyle: UIAlertController.Style.alert)
+                let okAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
+                myAlert.addAction(okAction)
+                self.present(myAlert, animated:true, completion:nil);
             }
-    }
-    /*
-    // MARK: - Navigation
+           
+            
+            if(UPassword != UPasswordCheck){
+                displayAlertMessage(userMessage: "비밀번호가 일치하지 않습니다.");
+                txtJoinPassword.text = ""
+                txtJoinPasswordCheck.text = ""
+            }
+            
+            if(UEmail=="" || UPassword=="" || UPasswordCheck=="" || UPostcode == "" || UAddr == "" || UName == "" || UTel == "" || lblEmailCheck.text == nil){
+            displayAlertMessage(userMessage: "빈칸과 이메일 중복체크를 확인해주세요");
+            return;
+        }else{
+            let insertModel = InsertModel()
+            let result = insertModel.insertItems(Email: UEmail, Name: UName, Tel: UTel, Postcode: UPostcode, Addr: UAddr, Password: UPassword, PasswordCheck: UPasswordCheck)
+            if result == true{
+                let resultAlert = UIAlertController(title: "완료", message: "가입이 완료되었습니다", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {ACTION in
+                    self.navigationController?.popViewController(animated: true)
+                })
+                resultAlert.addAction(onAction)
+                present(resultAlert, animated: true, completion: nil)
+                
+            }else{ // 에러일 경우
+                let resultAlert = UIAlertController(title: "실패", message: "에러가 발생 되었습니다.", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                resultAlert.addAction(onAction)
+                present(resultAlert, animated: true, completion: nil)
+                
+            }
+            
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    
-    @IBAction func btnJoin(_ sender: UIButton) {
-        let UEmail = txtJoinEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let UName = txtJoinName.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let UPassword = txtJoinPassword.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let UPasswordCheck = txtJoinPasswordCheck.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let UPostcode = txtJoinPostcode.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let UAddr = txtJoinAddr.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let UTel = txtJoinTel.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        func displayAlertMessage(userMessage: String){
-            let myAlert = UIAlertController(title: "알림", message: userMessage, preferredStyle: UIAlertController.Style.alert)
-            let okAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
-            myAlert.addAction(okAction)
-            self.present(myAlert, animated:true, completion:nil);
+            
         }
-       
-        
-        
-    if(UEmail=="" || UPassword=="" || UPasswordCheck=="" || UPostcode == "" || UAddr == "" || UName == "" || UTel == ""){
-        displayAlertMessage(userMessage: "빈칸이 없게 입력해주세요");
-        return;
-    }else{
-        let insertModel = InsertModel()
-        let result = insertModel.insertItems(Email: UEmail, Name: UName, Tel: UTel, Postcode: UPostcode, Addr: UAddr, Password: UPassword, PasswordCheck: UPasswordCheck)
-        if result == true{
-            let resultAlert = UIAlertController(title: "완료", message: "입력이 되었습니다", preferredStyle: UIAlertController.Style.alert)
-            let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {ACTION in
-                self.navigationController?.popViewController(animated: true)
-            })
-            resultAlert.addAction(onAction)
-            present(resultAlert, animated: true, completion: nil)
-            
-        }else{ // 에러일 경우
-            let resultAlert = UIAlertController(title: "실패", message: "에러가 발생 되었습니다.", preferredStyle: UIAlertController.Style.alert)
-            let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
-            resultAlert.addAction(onAction)
-            present(resultAlert, animated: true, completion: nil)
-            
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
         }
         
     }
-    if(UPassword != UPasswordCheck){
-        displayAlertMessage(userMessage: "비밀번호가 일치하지 않습니다.");
-        return;
-    }
-        
-    }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    
-}
+//
+//
+//    var feedItem: String = ""
+//
+//
+//    func itemDownloaded(items: String) {
+//        feedItem = items
+//        print("아이템다운로드\(feedItem)")
+//        if Int(feedItem) == 0{
+//            let resultAlert = UIAlertController(title: "성공", message: "사용가능한 아이디 입니다.", preferredStyle: UIAlertController.Style.alert)
+//            let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+//            resultAlert.addAction(onAction)
+//            present(resultAlert, animated: true, completion: nil)
+//            lblEmailCheck.text = ""
+//        }else if Int(feedItem) == 1{
+//            let resultAlert = UIAlertController(title: "실패", message: "사용중인 아이디 입니다.", preferredStyle: UIAlertController.Style.alert)
+//            let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+//            resultAlert.addAction(onAction)
+//            present(resultAlert, animated: true, completion: nil)
+//            lblEmailCheck.text = "사용 불가능한 이메일입니다."
+//            txtJoinEmail.text = ""
+//            }
+//    }
+//
+//
+//    @IBAction func btnJoinEmailCheck(_ sender: UIButton) {
+//        let emailCheckModel = EmailCheckModel()
+//                emailCheckModel.delegate = self
+//                emailCheckModel.checkItems(Email: txtJoinEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines))
+//
+//
+//    }
+//
+//        if feedItem.count == 0{
+//            let resultAlert = UIAlertController(title: "성공", message: "사용가능한 아이디 입니다.", preferredStyle: UIAlertController.Style.alert)
+//            let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+//            resultAlert.addAction(onAction)
+//            present(resultAlert, animated: true, completion: nil)
+//        }else{
+//            let resultAlert = UIAlertController(title: "실패", message: "사용중인 아이디 입니다.", preferredStyle: UIAlertController.Style.alert)
+//            let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {ACTION in
+//                self.navigationController?.popViewController(animated: true)
+//            })
+//            resultAlert.addAction(onAction)
+//            present(resultAlert, animated: true, completion: nil)
+//
+//            }
+//    }
+///*
+//   // MARK: - Navigation
+//
+//   // In a storyboard-based application, you will often want to do a little preparation before navigation
+//   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//       // Get the new view controller using segue.destination.
+//       // Pass the selected object to the new view controller.
+//   }
+//   */
+//
+//
+//   @IBAction func btnJoin(_ sender: UIButton) {
+//       let UEmail = txtJoinEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+//       let UName = txtJoinName.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+//       let UPassword = txtJoinPassword.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+//       let UPasswordCheck = txtJoinPasswordCheck.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+//       let UPostcode = txtJoinPostcode.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+//       let UAddr = txtJoinAddr.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+//       let UTel = txtJoinTel.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+//
+//       func displayAlertMessage(userMessage: String){
+//           let myAlert = UIAlertController(title: "알림", message: userMessage, preferredStyle: UIAlertController.Style.alert)
+//           let okAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
+//           myAlert.addAction(okAction)
+//           self.present(myAlert, animated:true, completion:nil);
+//       }
+//
+//
+//       if(UPassword != UPasswordCheck){
+//           displayAlertMessage(userMessage: "비밀번호가 일치하지 않습니다.");
+//           txtJoinPassword.text = ""
+//           txtJoinPasswordCheck.text = ""
+//       }
+//
+//       if(UEmail=="" || UPassword=="" || UPasswordCheck=="" || UPostcode == "" || UAddr == "" || UName == "" || UTel == "" || lblEmailCheck.text == nil){
+//       displayAlertMessage(userMessage: "빈칸과 이메일 중복체크를 확인해주세요");
+//       return;
+//   }else{
+//       let insertModel = InsertModel()
+//       let result = insertModel.insertItems(Email: UEmail, Name: UName, Tel: UTel, Postcode: UPostcode, Addr: UAddr, Password: UPassword, PasswordCheck: UPasswordCheck)
+//       if result == true{
+//           let resultAlert = UIAlertController(title: "완료", message: "가입이 완료되었습니다", preferredStyle: UIAlertController.Style.alert)
+//           let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {ACTION in
+//               self.navigationController?.popViewController(animated: true)
+//           })
+//           resultAlert.addAction(onAction)
+//           present(resultAlert, animated: true, completion: nil)
+//
+//       }else{ // 에러일 경우
+//           let resultAlert = UIAlertController(title: "실패", message: "에러가 발생 되었습니다.", preferredStyle: UIAlertController.Style.alert)
+//           let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+//           resultAlert.addAction(onAction)
+//           present(resultAlert, animated: true, completion: nil)
+//
+//       }
+//
+//   }
+//
+//   }
+//
+//   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//       self.view.endEditing(true)
+//   }
+//
+//
+//}
